35117ba6bb408ecea4dfa8d02afc1122c402121d
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-08-19 18:13:48 +0600

    fix the drive links

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function buildDriveImageUrl(link) {
   const match = link.match(/\/d\/(.+?)\//);
   if (match && match[1]) {
     const fileId = match[1];
-    return `https://drive.google.com/uc?export=view&id=${fileId}`;
+    return `https://drive.google.com/thumbnail?id=${fileId}&sz=s800`;
   }
   // if it's not a drive link, just return the link itself
   return link;
@@ -27,7 +27,7 @@ form.addEventListener("submit", (evt) => {
   card.className =
     "card text-slate-400 font-mono w-[320px] rounded-lg p-6 bg-slate-700 shadow-xl";
   imgContainer.className =
-    "image-container w-10 h-10 rounded-full overflow-hidden";
+    "image-container w-12 h-12 rounded-full overflow-hidden";
   img.className = "w-full h-full object-cover";
   h2.className = "font-bold mt-3 text-xl";
   h4.className = "font-semibold my-2 text-[18px]";
@@ -36,7 +36,6 @@ form.addEventListener("submit", (evt) => {
   // ✅ use the helper here
 
   img.src = buildDriveImageUrl(inputs[0].value);
-  console.log((img.src = buildDriveImageUrl(inputs[0].value)));
   h2.textContent = inputs[1].value;
   h4.textContent = inputs[2].value;
   p.textContent = `Works At ${inputs[3].value}`;
