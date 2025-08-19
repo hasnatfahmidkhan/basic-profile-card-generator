bb6226b475810ce4e133797b9031de00edef64e9
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-08-19 17:38:41 +0600

    google drive

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const mainContainer = document.querySelector("#card-container");
 const inputs = document.querySelectorAll("input");
 const form = document.querySelector("form");
+// Drive link checker Function
+function buildDriveImageUrl(link) {
+  const match = link.match(/\/d\/(.+?)\//);
+  if (match && match[1]) {
+    const fileId = match[1];
+    return `https://drive.google.com/uc?export=view&id=${fileId}`;
+  }
+  // if it's not a drive link, just return the link itself
+  return link;
+}
+
 form.addEventListener("submit", (evt) => {
-  evt.preventDefault(); // stop the form reload
+  evt.preventDefault();
 
   // Create fresh elements on every submit
   const card = document.createElement("div");
@@ -19,26 +30,25 @@ form.addEventListener("submit", (evt) => {
     "image-container w-10 h-10 rounded-full overflow-hidden";
   img.className = "w-full h-full object-cover";
   h2.className = "font-bold mt-3 text-xl";
-  h4.className = "font-semibold mt-2 text-[18px]";
+  h4.className = "font-semibold my-2 text-[18px]";
   p.className = "font-semibold";
 
-  // Set content
-  img.src = inputs[0].value;
+  // ✅ use the helper here
+
+  img.src = buildDriveImageUrl(inputs[0].value);
+  console.log((img.src = buildDriveImageUrl(inputs[0].value)));
   h2.textContent = inputs[1].value;
   h4.textContent = inputs[2].value;
   p.textContent = `Works At ${inputs[3].value}`;
 
-  // Build card
   imgContainer.appendChild(img);
   card.appendChild(imgContainer);
   card.appendChild(h2);
   card.appendChild(h4);
   card.appendChild(p);
 
-  // Add card to main container
   mainContainer.appendChild(card);
 
-  // Clear inputs
   inputs.forEach((inp) => {
     if (inp.type !== "submit") inp.value = "";
   });
